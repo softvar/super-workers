34722751ac29b63d00f9bd3a69164d3d2df03866
softvar <varun2902@gmail.com>
2017-05-09 21:04:46 +0530

    webpack configuration file setup

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+var webpack = require('webpack');
+var DashboardPlugin = require('webpack-dashboard/plugin');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var createVariants = require('parallel-webpack').createVariants;
+
+// Import the plugin:
+var path = require('path');
+var env = require('yargs').argv.mode;
+
+var libraryName = 'super-workers';
+
+var libVersion = JSON.stringify(require("./package.json").version);
+
+var libraryHeaderComment =  '\n' +
+  'super-workers ' + libVersion + '\n' +
+  'https://github.com/softvar/super-workers.js\n' +
+  'MIT LICENSE\n' +
+  '\n' +
+  'Copyright (C) 2017-2018 softvar - A project by Varun Malhotra(https://github.com/softvar)\n';
+
+
+var plugins = [
+  new webpack.BannerPlugin(libraryHeaderComment, { entryOnly: true })
+];
+var outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+} else {
+  plugins.push(new DashboardPlugin());
+}
+
+function createConfig(options) {
+  return {
+    entry: __dirname + '/src/index.js',
+    devtool: 'source-map',
+    output: {
+      path: __dirname + '/dist',
+      library: 'SuperWorkers',
+      filename: libraryName + (options.target ? '.' + options.target : '') + (env === 'build' ? '.min.js' : '.js'),
+      libraryTarget: options.target || 'umd',
+      umdNamedDefine: true
+    },
+    module: {
+      loaders: [
+        {
+          test: /(\.js)$/,
+          loader: 'babel',
+          exclude: /(node_modules|bower_components)/
+        },
+        {
+          test: /(\.js)$/,
+          loader: 'eslint-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    eslint: {
+      failOnWarning: false,
+      failOnError: false
+    },
+    resolve: {
+      root: path.resolve('./src'),
+      extensions: ['', '.js']
+    },
+    plugins: plugins
+  };
+}
+
+// At the end of the file:
+module.exports = createVariants({
+    target: ['this', '']
+}, createConfig);
