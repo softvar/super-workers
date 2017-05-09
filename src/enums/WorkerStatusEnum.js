a195f8225702e3ec9d236b75b57d9bdbc98264e1
softvar <varun2902@gmail.com>
2017-05-09 21:05:52 +0530

    specify know worker statuses

---
+++ src/enums/WorkerStatusEnum.js
@@ -0,0 +1,11 @@
+/**
+ * Enum for Worker status(still active / idle) used for client-worker-communication
+ * @type {Object}
+ */
+const WorkerStatusEnum = {
+  IDLE: 'idle',
+  ACTIVE: 'active',
+  TERMINATED: 'terminated'
+};
+
+export default WorkerStatusEnum;
